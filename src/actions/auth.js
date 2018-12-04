c11ca55eb7fbc7569bc8be1e793df02d73738ac1
Users <lana.chebotareva.95@mail.ru>
2018-12-04 16:41:18 +0300

    add actions/auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 import setAuthToken from '../setAuthToken';
 
+export const registerUser = (user, history) => dispatch => {
+    axios.post('/api/users/register', user)
+        .then(res => history.push('/login'))
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            });
+        });
+}
 
 export const loginUser = (user) => dispatch => {
     axios.post('/auth', user)
@@ -26,4 +36,10 @@ export const setCurrentUser = decoded => {
         type: SET_CURRENT_USER,
         payload: decoded
     }
+}
+export const logoutUser = (history) => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+    history.push('/login');
 }
